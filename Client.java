631e50398a13b4b0be9c1d756af6d99e4c7415a3
obada <gusbayab@student.gu.se>
2016-10-05 10:04:07 +0200

    File transfer error with big files

--- Client.java
+++ Client.java
@@ -1,16 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import org.omg.CORBA.portable.*;
+
+import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Array;
 import java.net.Socket;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Arrays;
+import java.util.Base64;
 
 /**
  * Created by obada on 2016-09-12.
  */
 public class Client {
 
-    BufferedReader input; // #1
-    PrintWriter output;// #2  are only for communication
+    InputStream input; // #1
+    OutputStream output;// #2
     Socket socket;// #3
 
     String folder; // location of the client's folder on the server
@@ -21,29 +27,209 @@ public Client(Socket socket)
 {
     this.socket = socket;
     try {
-        input = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-        output = new PrintWriter(this.socket.getOutputStream(),true); // true for auto flushing
+        input = socket.getInputStream();
+        output = socket.getOutputStream(); // true for auto flushing
     } catch (IOException e) {
         e.printStackTrace();
     }
 }
-public String getRequest()
-{
-    String str;
-    try {
-        if((str=input.readLine())!=null)
-            return (String) Server.decrypt(str);
-    } catch (IOException e) {
-        e.printStackTrace();
+
+    //A client sample code to connect and test out our server
+    public static void main(String[] args) throws Exception {
+        System.out.println("trying to connect");
+        Socket socket = new Socket("localhost", 3245);
+        System.out.println("connected!");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String request = "";
+        InputStream input = socket.getInputStream();
+        OutputStream output = socket.getOutputStream();
+        //....................................
+        while ((request = in.readLine()) != null) {
+            //decode the msg into base64
+            byte[] bytesEncoded = Base64.getEncoder().encode(request.getBytes());
+            //encrypt the byte array
+            byte[] encrypted = Server.encrypt(bytesEncoded);
+            //send the encrypted array
+            output.write(encrypted);
+            if (request.equals("hi")) {
+                //send a file
+                File file = new File("/home/obada/Desktop/Java.pdf");
+                byte[] temp = new byte[1024 * 1024];
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                byte[] fileBytes;
+                try (InputStream i = new FileInputStream(file);) {
+                    int count;
+                    while ((count = i.read(temp)) > 0) {
+                        //create a sub array of the full array
+                        byte[] tmp = Arrays.copyOfRange(temp, 0, count);
+                        //store the array in buffer
+                        buffer.write(tmp);
+                    }
+                    System.out.println("done with buffering");
+                    //store the whole file bytes in one array
+                    fileBytes = buffer.toByteArray();
+                    //encrypt it
+                    byte[] encrytped = Server.encrypt(fileBytes);
+                    //send it
+                    output.write(encrytped);
+                    output.write(new byte[]{-1});
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (request.equals("hi2")) {
+                File file = new File("/home/obada/Desktop/Java.pdf");
+                final FileChannel channel = new FileInputStream(file).getChannel();
+                MappedByteBuffer buff = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+                byte[] fileBytes = buff.array();
+                byte[] encr = Server.encrypt(fileBytes);
+                output.write(encr);
+                output.write(new byte[]{-1});
+            }
+        }
     }
+
+    /*
+        Receive a string encrypted with AES and encoded with base64
+     */
+    public String getRequest() throws Exception
+{
+    int count = 0;
+    byte[] temp = new byte[100];
+    if ((count = input.read(temp)) > 0) {
+        byte[] received = Arrays.copyOfRange(temp, 0, count);
+        //decrypt the byte array
+        byte[] decrypted = Server.decrypt(received);
+        //decoded with base64
+        byte[] valueDecoded = Base64.getDecoder().decode(decrypted);
+        //return the string representation
+        return new String(valueDecoded);
+    }
+    //client disconnected
     return "";
 }
-public void sendMsg(String msg)
+
+    /*
+        Send a string encoded with base64 and encrypted with AES 128 bit
+     */
+    public void sendMsg(String msg) throws Exception {
+        //decode the msg into base64
+        byte[] bytesEncoded = Base64.getEncoder().encode(msg.getBytes());
+        //encrypt the byte array
+        byte[] encrypted = Server.encrypt(bytesEncoded);
+        //send the encrypted array
+        output.write(encrypted);
+    }
+
+    /*
+        listen for requests and handle them
+     */
+    public void listen()
 {
-    String encryptedMsg = (String) Server.encrypt(msg);
-    output.println(encryptedMsg);
+    String request = "";
+    try {
+        while (!(request = getRequest()).equals("")) {
+            switch (request) {
+                //incoming file
+                case "hi":
+                    System.out.println("listening for a file");
+                    getFile("/home/obada/Desktop/new.pdf");
+                    break;
+                case "hi2":
+                    System.out.println("listening for a file");
+                    getFile("/home/obada/Desktop/new.pdf");
+                    break;
+                default:
+                    System.out.println(request);
+
+            }
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
 }
-public void close() throws IOException {
+
+    /*
+        send any kinda file to a client
+     */
+    public void sendFile(String location) {
+        File file = new File(location);
+        byte[] bytes = new byte[64 * 1024];
+        //the end signal of a file
+        byte end = -1;
+        try (InputStream input = new FileInputStream(file);) {
+            int count;
+            while ((count = input.read(bytes)) > 0) {
+                //create a sub array of the full array
+                bytes = Arrays.copyOfRange(bytes, 0, count);
+                //encrypt it
+                byte[] encrytped = Server.encrypt(bytes);
+                //send it
+                output.write(encrytped);
+            }
+            //send the end signal
+            output.write(end);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    get a file from a client
+    bug here with encryption: when you get the encrypted byte array divided in 2 arrays it's not possible to decrypt it
+    temp solution is to make the buffered byte array as big as the file we're sending or bigger
+ */
+    public void getFile(String location) {
+        System.out.println("getting a file");
+        File file = new File(location);
+        byte[] temp = new byte[1024 * 1024];
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        byte[] fileBytes;
+        try (OutputStream out = new FileOutputStream(file)) {
+            int count;
+            while ((count = input.read(temp)) > 0) {
+                //done for testing
+                System.out.println(count);
+                //tmp array with the downloaded bytes
+                byte[] tmp = Arrays.copyOfRange(temp, 0, count);
+                //if the end of the stream is the end signal then do the last iteration and quit the loop or you'll stuck here forever
+                if (tmp[tmp.length - 1] == -1) {
+                    System.out.println("last array of the file");
+                    //create a new array without the signal value
+                    tmp = Arrays.copyOfRange(tmp, 0, count - 1);
+                    //add it to buffer
+                    buffer.write(tmp);
+                    //add all the bytes to one array p.s: it's encrypted
+                    fileBytes = buffer.toByteArray();
+                    //decrypt it
+                    byte[] decrypted = Server.decrypt(fileBytes);
+                    //write it to the file
+                    out.write(decrypted);
+                    //quit the while loop
+                    break;
+                }
+                buffer.write(tmp);
+            }
+            System.out.println("file received");
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() throws IOException {
     input.close();
     output.close();
     socket.close();
--- Server.java
+++ Server.java
@@ -10,6 +10,7 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Obada on 2016-09-12.
@@ -33,79 +34,7 @@ public Server(int port)
     }
 
 }
-/*
-    Accept or decline a client connection "Connection in this phase is not encrypted"
- */
-public Client auth(Socket connection)
-{
-    try(OutputStream out = connection.getOutputStream();
-        InputStream in = connection.getInputStream())
-    {
-        String random="";
-        out.write(encrypt(random.getBytes()));//will generate a random string later
-        String answer="";
-        if(answer.equals(random))
-            return new Client(connection);
-    }
-    catch(EOFException e)
-    {
 
-    } catch (IOException e) {
-        e.printStackTrace();
-    } catch (Exception e) {
-        e.printStackTrace();
-    }
-    return null;
-}
-/*
-    send any kinda file to a client
- */
-public void sendFile(String location,Socket socket)
-{
-    File file = new File(location);// will encrypt later
-    byte[] bytes = new byte[64*1024];
-    try (InputStream input = new FileInputStream(file);
-         OutputStream output = socket.getOutputStream())
-    {
-        int count;
-        while((count=input.read(bytes))>0)
-        {
-            output.write(bytes,0,count);
-        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
-/*
-    get a file from a client
- */
-public void getFile(String location, Socket socket)
-{
-    File file = new File(location);//will decrypt later
-    byte[] bytes = new byte[64*1024];
-    try (InputStream input = socket.getInputStream();
-         OutputStream output = new FileOutputStream(file))
-    {
-        int count;
-        while((count=input.read(bytes))>0)
-        {
-            output.write(bytes,0,count);
-        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-}
-/*
-    send a msg to a client
- */
-public void sendMsg(Client client,String msg)
-{
-    client.sendMsg(msg);
-}
 /*
     Encrypt an object(msg,file...etc) with AES 128 bit encryption
  */
@@ -117,6 +46,7 @@ public static byte[] encrypt(byte[] Data) throws Exception
     byte[] encBytes = c.doFinal(Data);
     return encBytes;
 }
+
 /*
     Decrypt a received byte array from a client
  */
@@ -128,6 +58,7 @@ public static byte[] decrypt(byte[] encryptedData) throws Exception
     byte[] decValue = c.doFinal(encryptedData);
     return decValue;
 }
+
 /*
     Generate a key for your AES encryption
  */
@@ -138,32 +69,59 @@ private static Key generateKey()
     Key key = new SecretKeySpec(Key.getBytes(), "AES");
     return key;
 }
+
+    public static void main(String[] args) {
+        Server server = new Server(3245);
+        server.run();// example
+//    byte[] tmp = new byte[]{1,2,3};
+//    byte[] tmp2 = new byte[]{4,5,6};
+//    byte[] res=null;
+//    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+//    try {
+//        buffer.write(tmp2);
+//        buffer.write(tmp);
+//        buffer.write(new byte[]{-1});
+//        res=buffer.toByteArray();
+//        System.out.println(res[res.length-1]);
+//    } catch (IOException e) {
+//        e.printStackTrace();
+//    }
+
+
+    }
+
+/*
+    Accept or decline a client connection "Connection in this phase is not encrypted"
+ */
+public Client auth(Socket connection)
+{
+    try (OutputStream out = connection.getOutputStream();
+         InputStream in = connection.getInputStream())
+    {
+        String random = "";
+        out.write(encrypt(random.getBytes()));//will generate a random string later
+        String answer = "";
+        if (answer.equals(random))
+            return new Client(connection);
+    } catch (EOFException e) {
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    return null;
+}
+
 /*
     list all available files in a directory
  */
-public void ls(Client client,String location)
-{}
-/*
-    Listen to a client
- */
-public void listen(Client client)
-{
-    String request;
-    while((request=client.getRequest())!="")
-    {
-        handle(request);
-    }
+public void ls(Client client, String location) {
 }
-/*
-    handling a clients requests
- */
-public void handle(String str)
-{
 
-}
-/*
-    The main run method
- */
+    /*
+        The main run method
+     */
 public void run()
 {
 //***********************************************************************************************\\
@@ -171,25 +129,20 @@ public void run()
     {
      public void run()
      {
+         //listen for connections
          while(true)
          {
-             System.out.println("Listening for connections....");
              try {
-                 Client client;
-                 if((client = auth(serverSocket.accept()))!=null) {
-                     clients.add(client); // will work on authentication later
-                     System.out.println("Client connected..." + clients.get(clients.size() - 1).socket.getInetAddress());// get the IP address of the connected client
-                     new Thread() {
-                         public void run() {
-                             listen(clients.get(clients.size() - 1));
-                         }
-                     }.start();//listen to the newly connected client and execute
-                 }else
+                 System.out.println("listening for a client");
+                 Client cleint = new Client(serverSocket.accept());
+                 System.out.println("client connected");
+                 new Thread()
                  {
-                    System.out.println("Connection refused");
-                 }
-             } catch (IOException e)
-             {
+                     public void run() {
+                         cleint.listen();
+                     }
+                 }.start();
+             } catch (IOException e) {
                  e.printStackTrace();
              }
          }
@@ -197,10 +150,5 @@ public void run()
     }.start(); // A thread to listen for connections
 //***************************************************************************************************\\
 
-}
-public static void main(String[] args)
-{
-    Server server = new Server(3245);
-    server.run();// example
 }
 }
